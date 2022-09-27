21ddfe89f85e6ac0f614cca3da3a3c4fe3c3fb76
KevinAlejandroCol8 <kevin_gonzalez16@outlook.es>
2022-09-27 11:01:33 -0600

    Mantenimiento Principal

---
+++ Proyecto3/CapaVista/Menu.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace CapaVista
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.iNICIOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mANTENIMIENTOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aplicacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.módulosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.perfilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aSIGNACIONESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignacionesDeAplicacionesAUsuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignaciónDePerfilesAUsuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cAMBIOCONTRASEÑAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bITACORAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aYUDAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sedeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aluasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iNICIOToolStripMenuItem,
+            this.mANTENIMIENTOSToolStripMenuItem,
+            this.aSIGNACIONESToolStripMenuItem,
+            this.cAMBIOCONTRASEÑAToolStripMenuItem,
+            this.bITACORAToolStripMenuItem,
+            this.aYUDAToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1053, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // iNICIOToolStripMenuItem
+            // 
+            this.iNICIOToolStripMenuItem.Name = "iNICIOToolStripMenuItem";
+            this.iNICIOToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.iNICIOToolStripMenuItem.Text = "ABRIR";
+            // 
+            // mANTENIMIENTOSToolStripMenuItem
+            // 
+            this.mANTENIMIENTOSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuariosToolStripMenuItem,
+            this.aplicacionesToolStripMenuItem,
+            this.módulosToolStripMenuItem,
+            this.perfilesToolStripMenuItem,
+            this.sedeToolStripMenuItem,
+            this.aluasToolStripMenuItem});
+            this.mANTENIMIENTOSToolStripMenuItem.Name = "mANTENIMIENTOSToolStripMenuItem";
+            this.mANTENIMIENTOSToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.mANTENIMIENTOSToolStripMenuItem.Text = "CATALOGOS";
+            // 
+            // usuariosToolStripMenuItem
+            // 
+            this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.usuariosToolStripMenuItem.Text = "Maestros";
+            this.usuariosToolStripMenuItem.Click += new System.EventHandler(this.usuariosToolStripMenuItem_Click);
+            // 
+            // aplicacionesToolStripMenuItem
+            // 
+            this.aplicacionesToolStripMenuItem.Name = "aplicacionesToolStripMenuItem";
+            this.aplicacionesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aplicacionesToolStripMenuItem.Text = "Facultades";
+            // 
+            // módulosToolStripMenuItem
+            // 
+            this.módulosToolStripMenuItem.Name = "módulosToolStripMenuItem";
+            this.módulosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.módulosToolStripMenuItem.Text = "Carreras";
+            // 
+            // perfilesToolStripMenuItem
+            // 
+            this.perfilesToolStripMenuItem.Name = "perfilesToolStripMenuItem";
+            this.perfilesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.perfilesToolStripMenuItem.Text = "Curso";
+            // 
+            // aSIGNACIONESToolStripMenuItem
+            // 
+            this.aSIGNACIONESToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.asignacionesDeAplicacionesAUsuariosToolStripMenuItem,
+            this.asignaciónDePerfilesAUsuariosToolStripMenuItem});
+            this.aSIGNACIONESToolStripMenuItem.Name = "aSIGNACIONESToolStripMenuItem";
+            this.aSIGNACIONESToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.aSIGNACIONESToolStripMenuItem.Text = "PROCESOS";
+            // 
+            // asignacionesDeAplicacionesAUsuariosToolStripMenuItem
+            // 
+            this.asignacionesDeAplicacionesAUsuariosToolStripMenuItem.Name = "asignacionesDeAplicacionesAUsuariosToolStripMenuItem";
+            this.asignacionesDeAplicacionesAUsuariosToolStripMenuItem.Size = new System.Drawing.Size(247, 22);
+            this.asignacionesDeAplicacionesAUsuariosToolStripMenuItem.Text = "Asignaciones  Curso Maestros";
+            // 
+            // asignaciónDePerfilesAUsuariosToolStripMenuItem
+            // 
+            this.asignaciónDePerfilesAUsuariosToolStripMenuItem.Name = "asignaciónDePerfilesAUsuariosToolStripMenuItem";
+            this.asignaciónDePerfilesAUsuariosToolStripMenuItem.Size = new System.Drawing.Size(287, 22);
+            this.asignaciónDePerfilesAUsuariosToolStripMenuItem.Text = "Asignación de Perfiles a Usuarios";
+            // 
+            // cAMBIOCONTRASEÑAToolStripMenuItem
+            // 
+            this.cAMBIOCONTRASEÑAToolStripMenuItem.Name = "cAMBIOCONTRASEÑAToolStripMenuItem";
+            this.cAMBIOCONTRASEÑAToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.cAMBIOCONTRASEÑAToolStripMenuItem.Text = "INFORMES";
+            // 
+            // bITACORAToolStripMenuItem
+            // 
+            this.bITACORAToolStripMenuItem.Name = "bITACORAToolStripMenuItem";
+            this.bITACORAToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.bITACORAToolStripMenuItem.Text = "HERRAMIENTAS";
+            // 
+            // aYUDAToolStripMenuItem
+            // 
+            this.aYUDAToolStripMenuItem.Name = "aYUDAToolStripMenuItem";
+            this.aYUDAToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.aYUDAToolStripMenuItem.Text = "AYUDA";
+            // 
+            // sedeToolStripMenuItem
+            // 
+            this.sedeToolStripMenuItem.Name = "sedeToolStripMenuItem";
+            this.sedeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sedeToolStripMenuItem.Text = "Sede";
+            // 
+            // aluasToolStripMenuItem
+            // 
+            this.aluasToolStripMenuItem.Name = "aluasToolStripMenuItem";
+            this.aluasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aluasToolStripMenuItem.Text = "Aluas";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 626);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem iNICIOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mANTENIMIENTOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aplicacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem módulosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem perfilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aSIGNACIONESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignacionesDeAplicacionesAUsuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignaciónDePerfilesAUsuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cAMBIOCONTRASEÑAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bITACORAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aYUDAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sedeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aluasToolStripMenuItem;
+    }
+}
---
+++ Proyecto3/CapaVista/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void usuariosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Maestros b = new Maestros();
+            b.MdiParent = this;
+            b.Show();
+        }
+    }
+}
